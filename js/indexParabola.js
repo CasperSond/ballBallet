4a86d9c475eec4052853e40347bc48b33ffe197d
casper <casper.sndergaard@gmail.com>
2019-12-31 20:06:51 +0200

    300 ms

--- js/indexParabola.js
+++ js/indexParabola.js
@@ -2,5 +2,5 @@ import { setAttr, createSvgElement } from "./helpers.js";
 import { animate, createCircle } from "./parabola.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-  setInterval(createCircle, 100);
+  setInterval(createCircle, 300);
 });
